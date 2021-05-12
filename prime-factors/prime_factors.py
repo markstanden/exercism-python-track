ab3a1c5ef52157e5376e28a07a1e271f862b93ef
Mark Standen <mark@markstanden.dev>
2021-05-12 22:26:14 +0100

    added prime_factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,20 @@
-def factors(value):
-    pass
+def factors(value: int) -> list[int]:
+    """
+    Return a list of the prime factors of a number
+
+    :param value: the number to find the prime factors of
+    :type value: int
+    :return: an ordered list of the prime factors
+    :rtype: list[int]
+    """
+    # initialise
+    prime_factors = list()
+    prime = 2
+
+    while value != 1:
+        if value % prime == 0:
+            prime_factors.append(prime)
+            value //= prime
+        else:
+            prime += 1
+    return prime_factors
